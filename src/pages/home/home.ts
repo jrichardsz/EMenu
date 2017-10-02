02d337d9bd1920fbeb190ecd3c6ad456902e11ad
shamique <shamiquefarook@gmail.com>
2017-10-02 09:13:37 +0530

    style changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ViewMyCartPage } from "../view-my-cart/view-my-cart";
 import { MainMenuPage } from "../main-menu/main-menu";
+import { Platform} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -9,8 +10,19 @@ import { MainMenuPage } from "../main-menu/main-menu";
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, platform: Platform) {
+      platform.ready().then(() => {
 
+          let notificationOpenedCallback = (jsonData) => {
+            console.log(jsonData);
+          };
+
+          if (window['plugins']) {
+              window['plugins'].OneSignal.startInit("e6aa154b-e26a-4ff7-974d-1f75222e4f2b")
+                  .handleNotificationOpened(notificationOpenedCallback)
+                  .endInit();
+          }
+      });
   }
 
   ViewCheckOut = function(){
